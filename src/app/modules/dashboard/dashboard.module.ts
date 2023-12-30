985b40f9ceb923272dd746af4d3521169ca00f0b
galfermatheus <matheusgalvao03@gmail.com>
2023-12-30 08:30:16 -0300

    import do chart.js

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
@@ -9,7 +10,7 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { CookieService } from 'ngx-cookie-service';
+import { ChartModule } from 'primeng/chart';
 
 import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
 import { DASHBOARD_ROUTES } from './dashboard.routting';
@@ -31,6 +32,7 @@ import { DASHBOARD_ROUTES } from './dashboard.routting';
     ToolbarModule,
     CardModule,
     ToastModule,
+    ChartModule,
   ],
   providers: [MessageService, CookieService],
 })
