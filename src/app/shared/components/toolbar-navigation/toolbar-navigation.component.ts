216d7544771177a8dcec70c762be3c594006e86c
galfermatheus <matheusgalvao03@gmail.com>
2023-12-30 11:23:09 -0300

    criando botão e metodo para logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class ToolbarNavigationComponent {
 
+  constructor(
+    private cookie: CookieService,
+    private router: Router
+  ) {}
+
+  handleLogout(): void {
+    this.cookie.delete('USER_INFO');
+    void this.router.navigate(['/home']);
+  }
 }
