4726c62dd48b14bf1e111f4debee4fdbc7771674
galfermatheus <matheusgalvao03@gmail.com>
2024-01-02 17:15:52 -0300

    add service de categoria

---
+++ src/app/services/categories/categories.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private API_URL = environment.API_URL;
+  private JWT_TOKEN = this.cookie.get("USER_INFO");
+  private httpOptions = {
+    'Content-Type': 'application/json',
+    Autorization: `Bearer`
+  }
+  constructor(
+    private http: HttpClient,
+    private cookie: CookieService) { }
+
+  getAllCategories(): Observable<Array<GetCategoriesResponse>> {
+    return this.http.get<Array<GetCategoriesResponse>>(
+      `${this.API_URL}/categories`,
+      this.httpOptions
+    );
+  }
+}
