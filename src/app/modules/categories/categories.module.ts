22d82e90ad1fb166e81e13b3099156a24b1b645e
galfermatheus <matheusgalvao03@gmail.com>
2024-01-07 22:02:17 -0300

    add rotas e componentes para tela de categorias

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmationService } from 'primeng/api';
+
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+import { CATEGORIES_ROUTES } from './categories.routing';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CategoriesTableComponent } from './components/categories-table/categories-table.component';
+
+@NgModule({
+  declarations: [
+    CategoriesHomeComponent,
+    CategoriesTableComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROUTES),
+    SharedModule,
+    HttpClientModule,
+    //PRIMENG
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DynamicDialogModule,
+    ConfirmDialogModule,
+    TooltipModule,
+  ],
+  providers: [
+    DialogService,
+    ConfirmationService
+  ]
+})
+export class CategoriesModule { }
---
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
+
+@Component({
+  selector: 'app-categories-home',
+  templateUrl: './categories-home.component.html',
+  styleUrls: []
+})
+export class CategoriesHomeComponent implements OnInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject();
+  public categoriesDatas: Array<GetCategoriesResponse> = [];
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private dialogService: DialogService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.categoriesService
+    .getAllCategories()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        if (response.length > 0) {
+          this.categoriesDatas = response;
+        }
+      }, error: (err) => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar ccategoria',
+          life: 3000,
+        });
+        this.router.navigate(['/dashboard']);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
